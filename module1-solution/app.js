cc3843aba42384e08de822e51f25a0de9ea57306
mohamedbouziri <ge78qik@tum.de>
2023-10-19 16:54:40 +0200

    commiting changes

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,7 +1,40 @@
-(function() {
-   'use strict';
+(function () {
+    'use strict';
 
-    angular.module('LunchCheck',[])
-    .controller('LunchCheckController',LunchCheckController);
-    
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.textColorClass = '';
+        $scope.borderColorClass = '';
+        $scope.message = "";
+        $scope.checker = function () {
+            if (!$scope.lunchItems) {
+                $scope.message = "Please enter data";
+                $scope.textColorClass = 'text-danger';
+                $scope.borderColorClass = 'border-danger';
+                return;
+            }
+            $scope.lunchItems = $scope.lunchItems.split(',');
+            $scope.lunchItems = $scope.lunchItems.filter(function (lunchItem) {
+                return lunchItem.trim() !== '';
+            });
+            if ($scope.lunchItems.length === 0) {
+                $scope.message = "Please enter data";
+                $scope.textColorClass = 'text-danger';
+                $scope.borderColorClass = 'border-danger';
+            } else if ($scope.lunchItems.length <= 3) {
+                $scope.message = "Enjoy!";
+                $scope.textColorClass = 'text-success';
+                $scope.borderColorClass = 'border-success';
+            } else {
+                $scope.message = "Too much!";
+                $scope.textColorClass = 'text-success';
+                $scope.borderColorClass = 'border-success';
+            }
+        }
+
+    }
 })();
